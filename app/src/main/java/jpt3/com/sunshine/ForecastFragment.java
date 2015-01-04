3e80e3acb4c9032b8e520b545a40b0c4cd6f4c4d
Joseph Thomaschaske <joeythomaschaske@gmail.com>
2015-01-04 16:37:49 -0600

    Sprint 1 complete

--- app/src/main/java/jpt3/com/sunshine/ForecastFragment.java
+++ app/src/main/java/jpt3/com/sunshine/ForecastFragment.java
@@ -1,5 +1,6 @@
 package jpt3.com.sunshine;
 
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -12,45 +13,66 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class ForecastFragment extends Fragment {
+    ArrayAdapter<String> listViewAdapter = null;
 
     public ForecastFragment() {
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setHasOptionsMenu(true);
+
+        try{
+            super.onCreate(savedInstanceState);
+            setHasOptionsMenu(true);
+        } catch (Exception e){
+            Log.e("ForecastFragment Exception(onCreate)" + Thread.currentThread().getStackTrace()[1].getLineNumber(), e.getMessage(), e);
+        }
+
     }
 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        inflater.inflate(R.menu.forecastfragment, menu);
+
+        try{
+            inflater.inflate(R.menu.forecastfragment, menu);
+        } catch (Exception e){
+            Log.e("ForecastFragment Exception(onCreateOptionsMenu)" + Thread.currentThread().getStackTrace()[1].getLineNumber(), e.getMessage(), e);
+        }
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.action_refresh) {
-            FetchWeatherTask weatherTask = new FetchWeatherTask();
-            weatherTask.execute();
-            return true;
+        int id = -1;
+        FetchWeatherTask weatherTask = null;
+
+        try{
+            id = item.getItemId();
+            if (id == R.id.action_refresh) {
+                weatherTask = new FetchWeatherTask();
+                weatherTask.execute("53211");
+                return true;
+            }
+        } catch (Exception e){
+            Log.e("ForecastFragment Exception(onOptionsItemSelected)" + Thread.currentThread().getStackTrace()[1].getLineNumber(), e.getMessage(), e);
         }
         return super.onOptionsItemSelected(item);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        ArrayAdapter<String> listViewAdapter = null;
         ArrayList<String> weekForcast = null;
         ListView listView = null;
         View rootView = null;
@@ -69,24 +91,44 @@ public class ForecastFragment extends Fragment {
             listView.setAdapter(listViewAdapter);
         }
         catch (Exception e) {
-            Log.e("Sunshine Exception(onCreateView)" + Thread.currentThread().getStackTrace()[1].getLineNumber(), e.getMessage(), e);
+            Log.e("ForecastFragment Exception(onCreateView)" + Thread.currentThread().getStackTrace()[1].getLineNumber(), e.getMessage(), e);
         }
         return rootView;
     }
-    public class FetchWeatherTask extends AsyncTask<Void, Void, Void> {
+    public class FetchWeatherTask extends AsyncTask<String, Void, String[]> {
 
         @Override
-        protected Void doInBackground(Void... params) {
+        protected String[] doInBackground(String... params) {
+            int numDays = -1;
+            final String BASE_URI_PARAM = "http://api.openweathermap.org/data/2.5/forecast/daily?";
+            final String COUNTRY_CODE_PARAM = ",USA";
+            final String DAYS_PARAM = "cnt";
+            final String FORMAT_PARAM = "mode";
+            final String QUERY_PARAM = "q";
+            final String UNITS_PARAM = "units";
             String forecastJsonStr = "";
+            String format = "";
             String line = "";
+            String units = "";
             BufferedReader reader = null;
-            InputStream inputStream = null;
             HttpURLConnection urlConnection = null;
+            InputStream inputStream = null;
             StringBuffer buffer = null;
+            String [] result = null;
+            Uri builtUri = null;
             URL url = null;
 
             try {
-                url = new URL("http://api.openweathermap.org/data/2.5/forecast/daily?q=94043&mode=json&units=metric&cnt=7");
+                numDays = 7;
+                format = "json";
+                units = "metric";
+                builtUri = Uri.parse(BASE_URI_PARAM).buildUpon()
+                        .appendQueryParameter(QUERY_PARAM, params[0] +  COUNTRY_CODE_PARAM)
+                        .appendQueryParameter(FORMAT_PARAM, format)
+                        .appendQueryParameter(UNITS_PARAM, units)
+                        .appendQueryParameter(DAYS_PARAM, Integer.toString(numDays))
+                        .build();
+                url = new URL(builtUri.toString());
                 urlConnection = (HttpURLConnection) url.openConnection();
                 urlConnection.setRequestMethod("GET");
                 urlConnection.connect();
@@ -96,23 +138,94 @@ public class ForecastFragment extends Fragment {
                     return null;
                 }
                 reader = new BufferedReader(new InputStreamReader(inputStream));
-                while ((line = reader.readLine()) != null) {
-                    buffer.append(line + "\n");
-                }
+                while ((line = reader.readLine()) != null)
+                    buffer.append(line).append("\n");
                 if (buffer.length() == 0) {
                     return null;
                 }
                 forecastJsonStr = buffer.toString();
-                if (urlConnection != null) {
-                    urlConnection.disconnect();
-                }
-                if (reader != null) {
-                    reader.close();
-                }
+                result = getWeatherDataArray(forecastJsonStr);
+                urlConnection.disconnect();
+                reader.close();
             } catch (Exception e) {
-                Log.e("Sunshine Exception(doInBackground) " + Thread.currentThread().getStackTrace()[1].getLineNumber(), e.getMessage(), e);
+                Log.e("ForecastFragment Exception(doInBackground) " + Thread.currentThread().getStackTrace()[1].getLineNumber(), e.getMessage(), e);
             }
-            return null;
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String[] result) {
+
+            try{
+                listViewAdapter.clear();
+                for(String day: result){
+                    listViewAdapter.add(day);
+                }
+            } catch (Exception e){
+                Log.e("ForecastFragment Exception(onPostExecute) " + Thread.currentThread().getStackTrace()[3].getLineNumber(), e.getMessage(), e);
+            }
+
+        }
+
+        private String[] getWeatherDataArray(String forecastJsonStr) throws JSONException {
+            int index = -1;
+            String day = "";
+            String description = "";
+            String highAndLow = "";
+            JSONArray weatherArray = null;
+            JSONObject dayJson = null;
+            JSONObject forecastJson = null;
+            JSONObject temperatureObject = null;
+            JSONObject weatherObject = null;
+            String[] results = null;
+
+            try{
+                forecastJson = new JSONObject(forecastJsonStr);
+                weatherArray = forecastJson.getJSONArray("list");
+                results = new String[7];
+                for(index = 0; index < weatherArray.length(); ++index){
+                    dayJson = weatherArray.getJSONObject(index);
+                    day = getReadableDateString(dayJson.getLong("dt"));
+                    weatherObject = dayJson.getJSONArray("weather").getJSONObject(0);
+                    description = weatherObject.getString("main");
+                    temperatureObject = dayJson.getJSONObject("temp");
+                    highAndLow = formatHighLows(temperatureObject.getDouble("max"), temperatureObject.getDouble("min"));
+                    results[index] = day + "-" + description + "-" + highAndLow;
+                }
+            }catch (Exception e){
+                Log.e("ForecastFragment Exception(getWeatherDataArray) " + Thread.currentThread().getStackTrace()[1].getLineNumber(), e.getMessage(), e);
+            }
+            return results;
+        }
+
+        private String getReadableDateString(long time){
+            String formatedDate = "";
+            Date date = null;
+            SimpleDateFormat format = null;
+
+            try{
+                date = new Date(time * 1000);
+                format = new SimpleDateFormat("E, MMM d");
+                formatedDate = format.format(date);
+            }catch (Exception e){
+                Log.e("ForecastFragment Exception(getReadableDateString) " + Thread.currentThread().getStackTrace()[1].getLineNumber(), e.getMessage(), e);
+            }
+            return formatedDate;
+        }
+
+        private String formatHighLows(double high, double low) {
+            long roundedHigh = -1;
+            long roundedLow = -1;
+            String highLowStr = "";
+
+            try{
+                roundedHigh = Math.round(high);
+                roundedLow = Math.round(low);
+                highLowStr = roundedHigh + "/" + roundedLow;
+            }catch (Exception e){
+                Log.e("ForecastFragment Exception(formatHighLows) " + Thread.currentThread().getStackTrace()[1].getLineNumber(), e.getMessage(), e);
+            }
+            return highLowStr;
         }
     }
 }
--- app/src/main/java/jpt3/com/sunshine/MainActivity.java
+++ app/src/main/java/jpt3/com/sunshine/MainActivity.java
@@ -1,6 +1,7 @@
 package jpt3.com.sunshine;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -9,24 +10,43 @@ public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new ForecastFragment())
-                    .commit();
+
+        try{
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            if (savedInstanceState == null) {
+                getSupportFragmentManager().beginTransaction()
+                        .add(R.id.container, new ForecastFragment())
+                        .commit();
+            }
+        } catch (Exception e){
+            Log.e("MainActivity Exception(onCreate)" + Thread.currentThread().getStackTrace()[1].getLineNumber(), e.getMessage(), e);
         }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        try{
+            getMenuInflater().inflate(R.menu.menu_main, menu);
+        } catch (Exception e){
+            Log.e("MainActivity Exception(onCreateOptionsMenu)" + Thread.currentThread().getStackTrace()[1].getLineNumber(), e.getMessage(), e);
+        }
+
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        return id == R.id.action_settings || super.onOptionsItemSelected((item));
+        int id = -1;
+
+        try{
+            id = item.getItemId();
+            if (id == R.id.action_settings)
+                return true;
+        } catch (Exception e){
+            Log.e("MainActivity Exception(onCreateOptionsMenu)" + Thread.currentThread().getStackTrace()[1].getLineNumber(), e.getMessage(), e);
+        }
+        return super.onOptionsItemSelected((item));
     }
 }
