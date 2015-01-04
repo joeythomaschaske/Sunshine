062853d4720a477df90e2836706a94ce87b8f3dd
Joseph Thomaschaske <joeythomaschaske@gmail.com>
2015-01-04 14:16:53 -0600

    Refactor

--- app/src/main/java/jpt3/com/sunshine/ForecastFragment.java
+++ app/src/main/java/jpt3/com/sunshine/ForecastFragment.java
@@ -14,7 +14,6 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
@@ -51,11 +50,10 @@ public class ForecastFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        ArrayList<String> weekForcast = null;
         ArrayAdapter<String> listViewAdapter = null;
+        ArrayList<String> weekForcast = null;
         ListView listView = null;
         View rootView = null;
-        String forecastJsonStr = "";
 
         try {
             rootView = inflater.inflate(R.layout.fragment_main, container, false);
@@ -77,67 +75,42 @@ public class ForecastFragment extends Fragment {
     }
     public class FetchWeatherTask extends AsyncTask<Void, Void, Void> {
 
-        private final String LOG_TAG = FetchWeatherTask.class.getSimpleName();
-
         @Override
         protected Void doInBackground(Void... params) {
-            // These two need to be declared outside the try/catch
-            // so that they can be closed in the finally block.
-            HttpURLConnection urlConnection = null;
+            String forecastJsonStr = "";
+            String line = "";
             BufferedReader reader = null;
-
-            // Will contain the raw JSON response as a string.
-            String forecastJsonStr = null;
+            InputStream inputStream = null;
+            HttpURLConnection urlConnection = null;
+            StringBuffer buffer = null;
+            URL url = null;
 
             try {
-                // Construct the URL for the OpenWeatherMap query
-                // Possible parameters are avaiable at OWM's forecast API page, at
-                // http://openweathermap.org/API#forecast
-                URL url = new URL("http://api.openweathermap.org/data/2.5/forecast/daily?q=94043&mode=json&units=metric&cnt=7");
-
-                // Create the request to OpenWeatherMap, and open the connection
+                url = new URL("http://api.openweathermap.org/data/2.5/forecast/daily?q=94043&mode=json&units=metric&cnt=7");
                 urlConnection = (HttpURLConnection) url.openConnection();
                 urlConnection.setRequestMethod("GET");
                 urlConnection.connect();
-
-                // Read the input stream into a String
-                InputStream inputStream = urlConnection.getInputStream();
-                StringBuffer buffer = new StringBuffer();
+                inputStream = urlConnection.getInputStream();
+                buffer = new StringBuffer();
                 if (inputStream == null) {
-                    // Nothing to do.
                     return null;
                 }
                 reader = new BufferedReader(new InputStreamReader(inputStream));
-
-                String line;
                 while ((line = reader.readLine()) != null) {
-                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
-                    // But it does make debugging a *lot* easier if you print out the completed
-                    // buffer for debugging.
                     buffer.append(line + "\n");
                 }
-
                 if (buffer.length() == 0) {
-                    // Stream was empty.  No point in parsing.
                     return null;
                 }
                 forecastJsonStr = buffer.toString();
-            } catch (IOException e) {
-                Log.e(LOG_TAG, "Error ", e);
-                // If the code didn't successfully get the weather data, there's no point in attemping
-                // to parse it.
-                return null;
-            } finally {
                 if (urlConnection != null) {
                     urlConnection.disconnect();
                 }
                 if (reader != null) {
-                    try {
-                        reader.close();
-                    } catch (final IOException e) {
-                        Log.e(LOG_TAG, "Error closing stream", e);
-                    }
+                    reader.close();
                 }
+            } catch (Exception e) {
+                Log.e("Sunshine Exception(doInBackground) " + Thread.currentThread().getStackTrace()[1].getLineNumber(), e.getMessage(), e);
             }
             return null;
         }
